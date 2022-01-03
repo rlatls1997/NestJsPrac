a5218e03fc9e3dfbac8450ad4793aab30efbaecf
sinhyuk <rlatls1997@gmail.com>
2022-01-03 22:23:59 +0900

    chore : prettier, eslint 설정

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    //'airbnb',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
