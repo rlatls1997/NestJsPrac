b5f4ab07a904914f4c68ad1209e45d5fdc03a739
sinhyuk <rlatls1997@gmail.com>
2022-01-04 19:09:35 +0900

    feat : auth guard 추가

--- backend/src/cats/cats.controller.ts
+++ backend/src/cats/cats.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('cats')
+@UseGuards(AuthGuard)
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
